ce60cd26c91df830a7424a26f35be0ebe6155252
richalzackyaflacha <richalzackyi@gmail.com>
2022-04-03 23:35:42 +0700

    my main Portfolio

--- config/app.php
+++ config/app.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Facade;
 
 return [
 
+    "log"=>"errorlog",
     /*
     |--------------------------------------------------------------------------
     | Application Name
